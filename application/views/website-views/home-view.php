948354815f6123a7aba19106479d39ed33fd3e9c
ksykko <sagala.krch@gmail.com>
2022-09-21 21:36:26 +0800

    Added landing page

--- application/views/website-views/home-view.php
+++ application/views/website-views/home-view.php
@@ -1,7 +1,7 @@
 <section id="home-banner" class="py-4 py-xl-5 " style="background: url('<?= base_url('/assets/img/others/banner-epmcblue.png') ?>') center / cover no-repeat;">
     <div class="container h-100">
         <div class="row h-100">
-            <div class="col-md-10 col-lg-12 col-xl-8 col-xxl-12 text-center d-flex d-sm-flex d-md-flex mx-auto justify-content-md-start justify-content-xxl-start align-items-xxl-start">
+            <div class="col-md-10 col-lg-12 col-xl-8 col-xxl-12 text-center d-flex d-sm-flex d-md-flex mx-auto justify-content-md-start justify-content-xxl-start align-items-xxl-start col-banner">
                 <div class="headings">
                     <h1 class="h1-first">Welcome to</h1>
                     <h1 class="h1-second">Pagtakhan Medical Clinic's<br>Website</h1>
@@ -15,12 +15,10 @@
         <div class="row">
             <div class="col-md-6">
                 <h1 class="h1-third">Pagtakhan Medical Clinic Web and Mobile Application</h1>
-                <p>Pagtakhan Medical Clinic now has a web and mobile application where patients can view their doctor’s schedule, patient’s general data, history of consultation, and lab reports. The website also has a chatbot where patient’s can ask about their concerns regarding the clinic.<br><br></p>
+                <p class="home-desc">Pagtakhan Medical Clinic now has a web and mobile application where patients can view their doctor’s schedule, patient’s general data, history of consultation, and lab reports. The website also has a chatbot where patient’s can ask about their concerns regarding the clinic.<br><br></p>
             </div>
             <div class="col-md-6"><img class="img-fluid" src=" <?= base_url('/assets/img/others/Home.png') ?> "></div>
         </div>
-        <p class="signature ">Copyright © ePMC 2022</a>
-        </p>
     </div>
 </section>
 
@@ -32,9 +30,9 @@
                 <p class="w-lg-50">Curae hendrerit donec commodo hendrerit egestas tempus, turpis facilisis nostra nunc. Vestibulum dui eget ultrices.</p>
             </div>
         </div>
-        <div class="row gy-4 row-cols-1 row-cols-md-4 d-md-flex d-lg-flex d-xxl-flex justify-content-md-center align-items-md-center justify-content-lg-center align-items-lg-center justify-content-xxl-center" style="margin-bottom: 40px;">
+        <div class="row gy-4 row-cols-1 row-cols-md-4 d-md-flex d-lg-flex d-xxl-flex justify-content-md-center align-items-md-center justify-content-lg-center align-items-lg-center justify-content-xxl-center row-cards">
             <div class="col">
-                <div class="card border-0 shadow-none">
+                <div class="card border-0 shadow-sm">
                     <div class="card-body text-center d-flex flex-column align-items-center p-0"><img class="rounded-circle mb-3 fit-cover img-avatar" width="130" height="130" src="<?= base_url('/assets/img/avatars/avatardoc.png') ?>" />
                         <h5 class="fw-bold text-primary card-title mb-0">Dr. Luis Pagtakhan<br /></h5>
                         <p class="text-muted card-text mb-2">Owner / Family Medicine</p>
@@ -53,7 +51,7 @@
                 </div>
             </div>
             <div class="col">
-                <div class="card border-0 shadow-none">
+                <div class="card border-0 shadow-sm">
                     <div class="card-body text-center d-flex flex-column align-items-center p-0"><img class="rounded-circle mb-3 fit-cover img-avatar" width="130" height="130" src="<?= base_url('/assets/img/avatars/avatarcashier.png') ?>" />
                         <h5 class="fw-bold text-primary card-title mb-0">Ms. Emelie Pagtakhan<br /></h5>
                         <p class="text-muted card-text mb-2">Owner / General Manager</p>
@@ -72,9 +70,9 @@
                 </div>
             </div>
         </div>
-        <div class="row gy-4 row-cols-1 row-cols-md-4 d-lg-flex d-xxl-flex justify-content-lg-center align-items-lg-center justify-content-xxl-center align-items-xxl-center" style="margin-bottom: 40px;position: static;display: none;">
+        <div class="row gy-4 row-cols-1 row-cols-md-4 d-lg-flex d-xxl-flex justify-content-lg-center align-items-lg-center justify-content-xxl-center align-items-xxl-center row-cards">
             <div class="col">
-                <div class="card border-0 shadow-none">
+                <div class="card border-0 shadow-sm">
                     <div class="card-body text-center d-flex flex-column align-items-center p-0"><img class="rounded-circle mb-3 fit-cover img-avatar" width="130" height="130" src="<?= base_url('/assets/img/avatars/avatarfemaledoc.png') ?>" />
                         <h5 class="fw-bold text-primary card-title mb-0">Dr. Jaymie Pagtakhan<br /></h5>
                         <p class="text-muted card-text mb-2">Obstetrics and Gynecology<br /></p>
@@ -93,7 +91,7 @@
                 </div>
             </div>
             <div class="col">
-                <div class="card border-0 shadow-none">
+                <div class="card border-0 shadow-sm">
                     <div class="card-body text-center d-flex flex-column align-items-center p-0"><img class="rounded-circle mb-3 fit-cover img-avatar" width="130" height="130" src="<?= base_url('/assets/img/avatars/avatardoc2.png') ?>" />
                         <h5 class="fw-bold text-primary card-title mb-0">Dr. Jass Hussein<br /></h5>
                         <p class="text-muted card-text mb-2">Orthopedics<br /></p>
@@ -112,8 +110,8 @@
                 </div>
             </div>
             <div class="col">
-                <div class="card border-0 shadow-none">
-                    <div class="card-body text-center d-flex flex-column align-items-center p-0"><img class="rounded-circle mb-3 fit-cover img-avatar" width="130" height="130" src="<?= base_url('/assets/img/avatars/avatardoc3.png') ?>" />
+                <div class="card border-0 shadow-sm">
+                    <div class="card-body text-center d-flex flex-column align-items-center p-0"><img class="rounded-circle mb-3 fit-cover img-avatar" width="130" height="130" src="<?= base_url('assets/img/avatars/avatardoc3.png') ?>" />
                         <h5 class="fw-bold text-primary card-title mb-0">Dr. Miguel Pagtakhan<br /></h5>
                         <p class="text-muted card-text mb-2">Family Medicine</p>
                         <ul class="list-inline fs-6 text-muted w-100 mb-0">
@@ -131,9 +129,9 @@
                 </div>
             </div>
         </div>
-        <div class="row gy-4 row-cols-1 row-cols-md-4 d-lg-flex d-xxl-flex justify-content-lg-center align-items-lg-center justify-content-xxl-center align-items-xxl-center" style="margin-bottom: 40px;position: static;display: none;">
+        <div class="row gy-4 row-cols-1 row-cols-md-4 d-lg-flex d-xxl-flex justify-content-lg-center align-items-lg-center justify-content-xxl-center align-items-xxl-center row-cards">
             <div class="col">
-                <div class="card border-0 shadow-none">
+                <div class="card border-0 shadow-sm">
                     <div class="card-body text-center d-flex flex-column align-items-center p-0"><img class="rounded-circle mb-3 fit-cover img-avatar" width="130" height="130" src="<?= base_url('/assets/img/avatars/avatarsecretary.png') ?>" />
                         <h5 class="fw-bold text-primary card-title mb-0">Ms. Genalyn Tupan<br /></h5>
                         <p class="text-muted card-text mb-2">Secretary<br /></p>
@@ -152,7 +150,7 @@
                 </div>
             </div>
             <div class="col">
-                <div class="card border-0 shadow-none">
+                <div class="card border-0 shadow-sm">
                     <div class="card-body text-center d-flex flex-column align-items-center p-0"><img class="rounded-circle mb-3 fit-cover img-avatar" width="130" height="130" src="<?= base_url('/assets/img/avatars/avatarmanager.png') ?>" />
                         <h5 class="fw-bold text-primary card-title mb-0">Ms. Karen Tumayo<br /></h5>
                         <p class="text-muted card-text mb-2">Secretary<br /></p>
@@ -171,7 +169,7 @@
                 </div>
             </div>
             <div class="col">
-                <div class="card border-0 shadow-none">
+                <div class="card border-0 shadow-sm">
                     <div class="card-body text-center d-flex flex-column align-items-center p-0"><img class="rounded-circle mb-3 fit-cover img-avatar" width="130" height="130" src="<?= base_url('/assets/img/avatars/avatar1.png') ?>" />
                         <h5 class="fw-bold text-primary card-title mb-0">Ms. Marissa Samaniego<br /></h5>
                         <p class="text-muted card-text mb-2">Nurse</p>
@@ -190,9 +188,130 @@
                 </div>
             </div>
         </div>
-    </div>
+        <div class="row gy-4 row-cols-1 row-cols-md-4 d-flex d-md-flex d-lg-flex d-xxl-flex justify-content-center align-items-center justify-content-md-center align-items-md-center justify-content-lg-center align-items-lg-center justify-content-xxl-center align-items-xxl-center row-cards">
+            <div class="col">
+                <div class="card border-0 shadow-sm">
+                    <div class="card-body text-center d-flex flex-column align-items-center p-0"><img class="rounded-circle mb-3 fit-cover img-avatar" width="130" height="130" src="<?= base_url('/assets/img/avatars/avatarpharmacyassistant.png') ?>" />
+                        <h5 class="fw-bold text-primary card-title mb-0">Ms. Carina Tumimbang<br /></h5>
+                        <p class="text-muted card-text mb-2">Pharmacy Assistant<br /></p>
+                        <ul class="list-inline fs-6 text-muted w-100 mb-0">
+                            <li class="list-inline-item text-center">
+                                <div class="d-flex flex-column align-items-center"></div>
+                            </li>
+                            <li class="list-inline-item text-center">
+                                <div class="d-flex flex-column align-items-center"></div>
+                            </li>
+                            <li class="list-inline-item text-center">
+                                <div class="d-flex flex-column align-items-center"></div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card border-0 shadow-sm">
+                    <div class="card-body text-center d-flex flex-column align-items-center p-0"><img class="rounded-circle mb-3 fit-cover img-avatar" width="130" height="130" src="<?= base_url('/assets/img/avatars/avatarmanager.png') ?>" />
+                        <h5 class="fw-bold text-primary card-title mb-0">Ms. Melody Gayondato<br /></h5>
+                        <p class="text-muted card-text mb-2">Cashier<br /></p>
+                        <ul class="list-inline fs-6 text-muted w-100 mb-0">
+                            <li class="list-inline-item text-center">
+                                <div class="d-flex flex-column align-items-center"></div>
+                            </li>
+                            <li class="list-inline-item text-center">
+                                <div class="d-flex flex-column align-items-center"></div>
+                            </li>
+                            <li class="list-inline-item text-center">
+                                <div class="d-flex flex-column align-items-center"></div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row d-lg-flex justify-content-lg-center align-items-lg-center mb-4 mb-lg-5 row-dev">
+            <div class="col-md-8 col-xl-6 text-center mx-auto">
+                <h2>Developers</h2>
+                <p class="w-lg-50">The developers behind ePMC</p>
+            </div>
+        </div>
+        <div class="row gy-4 row-cols-1 row-cols-md-4 d-lg-flex justify-content-lg-center align-items-lg-center">
+            <div class="col">
+                <div class="card border-0 shadow-sm">
+                    <div class="card-body text-center d-flex flex-column align-items-center p-0"><img class="rounded-circle mb-3 fit-cover img-avatar" width="130" height="130" src="<?= base_url('/assets/img/avatars/PJ.png') ?>" />
+                        <h5 class="fw-bold text-primary card-title mb-0">Manuel, Paul Jeremy C.</h5>
+                        <p class="text-muted card-text mb-2">Project Manager</p>
+                        <ul class="list-inline fs-6 text-muted w-100 mb-0">
+                            <li class="list-inline-item text-center">
+                                <div class="d-flex flex-column align-items-center"></div>
+                            </li>
+                            <li class="list-inline-item text-center">
+                                <div class="d-flex flex-column align-items-center"></div>
+                            </li>
+                            <li class="list-inline-item text-center">
+                                <div class="d-flex flex-column align-items-center"></div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card border-0 shadow-sm">
+                    <div class="card-body text-center d-flex flex-column align-items-center p-0"><img class="rounded-circle mb-3 fit-cover img-avatar" width="130" height="130" src="<?= base_url('/assets/img/avatars/Venge.png') ?>" />
+                        <h5 class="fw-bold text-primary card-title mb-0">Angot, Vengeline B.</h5>
+                        <p class="text-muted card-text mb-2">Creative Director</p>
+                        <ul class="list-inline fs-6 text-muted w-100 mb-0">
+                            <li class="list-inline-item text-center">
+                                <div class="d-flex flex-column align-items-center"></div>
+                            </li>
+                            <li class="list-inline-item text-center">
+                                <div class="d-flex flex-column align-items-center"></div>
+                            </li>
+                            <li class="list-inline-item text-center">
+                                <div class="d-flex flex-column align-items-center"></div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card border-0 shadow-sm">
+                    <div class="card-body text-center d-flex flex-column align-items-center p-0"><img class="rounded-circle mb-3 fit-cover img-avatar" width="130" height="130" src="<?= base_url('/assets/img/avatars/Ken.jpg') ?>" />
+                        <h5 class="fw-bold text-primary card-title mb-0">Sagala, Kendrick Andre A,</h5>
+                        <p class="text-muted card-text mb-2">Technical Lead</p>
+                        <ul class="list-inline fs-6 text-muted w-100 mb-0">
+                            <li class="list-inline-item text-center">
+                                <div class="d-flex flex-column align-items-center"></div>
+                            </li>
+                            <li class="list-inline-item text-center">
+                                <div class="d-flex flex-column align-items-center"></div>
+                            </li>
+                            <li class="list-inline-item text-center">
+                                <div class="d-flex flex-column align-items-center"></div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card border-0 shadow-sm">
+                    <div class="card-body text-center d-flex flex-column align-items-center p-0"><img class="rounded-circle mb-3 fit-cover img-avatar" width="130" height="130" src="<?= base_url('/assets/img/avatars/Rian.jpg') ?>" />
+                        <h5 class="fw-bold text-primary card-title mb-0">Sandoval, Mary Angel R.</h5>
+                        <p class="text-muted card-text mb-2">QA Specialist</p>
+                        <ul class="list-inline fs-6 text-muted w-100 mb-0">
+                            <li class="list-inline-item text-center">
+                                <div class="d-flex flex-column align-items-center"></div>
+                            </li>
+                            <li class="list-inline-item text-center">
+                                <div class="d-flex flex-column align-items-center"></div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <p class="signature ">Copyright © ePMC 2022</p>
 </section>
-
+ 
 
 <section id="contact">
     <div class="container ">
