f80a161870d61557f41fb756b671699505a7ba1d
Vengeline Angot <201910550@fit.edu.ph>
2022-09-21 21:41:13 +0800

    updated assets folder

--- application/views/include-website/scripts.php
+++ application/views/include-website/scripts.php
@@ -4,6 +4,7 @@
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js'></script>
 <script src="<?= base_url('/assets/js/website-scripts.js') ?>"></script>
+<script src="<?= base_url('/assets/js/dashboard-header.js') ?>"></script>
 <script src="https://account.snatchbot.me/script.js"></script>
 <script>
     window.sntchChat.Init(270244)
