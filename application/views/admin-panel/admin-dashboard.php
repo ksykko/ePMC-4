50481967353ed645fe22c60fb795abcfc9ced204
Vengeline Angot <201910550@fit.edu.ph>
2022-09-22 11:39:25 +0800

    updated admin dashboard

--- application/views/admin-panel/admin-dashboard.php
+++ application/views/admin-panel/admin-dashboard.php
@@ -33,13 +33,12 @@
             <div class="profile-box">
                 <img style="width: 150px;" class="img-fluid" name="avatar" src="<?= base_url('/assets/img/avatars/avatar1.png') ?>"><br><br>
                 <label for="avatar">Hello, <br> Vengeline B. Angot</label><br>
-                <label for="avatar" class="role">ADMIN</label>
+                <label for="avatar" class="role">ADMIN</label><br>
             </div>
 
             <footer class="sidebar-footer">
                 Terms of Use | Privacy Policy
             </footer>
-
         </div> 
     
 
@@ -55,7 +54,7 @@
 
                 <div class="box-wrapper">
                     <div class="inner-content">
-                        <i class="fas fa-notes-medical"></i>
+                        <i class="fas fa-archive"></i>
                         <label for="" class="number-label">43</label>
                         <label for="" class="description-label">Total no. of Inventory Items</label>
                     </div>    
@@ -63,7 +62,7 @@
 
                 <div class="box-wrapper">
                     <div class="inner-content">
-                        <i class="fas fa-notes-medical"></i>
+                        <i class="fas fa-user-alt"></i>
                         <label for="" class="number-label">97</label>
                         <label for="" class="description-label">Total no. of User Accounts</label>
                     </div>    
@@ -71,7 +70,7 @@
 
                 <div class="box-wrapper">
                     <div class="inner-content">
-                        <i class="fas fa-notes-medical"></i>
+                        <i class="fas fa-address-book"></i>
                         <label for="" class="number-label">5</label>
                         <label for="" class="description-label">Total no. of New Patients today</label>
                     </div>    
@@ -86,13 +85,13 @@
                                 <div class='tr-table' style='margin-bottom:10px;'>
                                     <table>
                                         <tr>
-                                            <td style='width: 5%;'>
+                                            <td style='width: auto'>
                                                 <i class='fas fa-exclamation-circle'></i>
                                             </td>
-                                            <td style='width: 80%;'>
+                                            <td style='width: auto'>
                                                 Has added a new patient record.
                                             </td>
-                                            <td style='width: 15%;'>
+                                            <td style='width: auto'>
                                                 02/09/2021  09:00 AM
                                             </td>    
                                         </tr>  
