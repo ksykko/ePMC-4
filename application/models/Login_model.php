4f58ae5db19f722c5a0c5a72c4770ae5ba95a3d8
ksykko <sagala.krch@gmail.com>
2022-09-26 17:47:47 +0800

    Added icons

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -21,6 +21,16 @@ class Login_model extends CI_Model
         //     ->where('ta.password', $pass)
         //     ->get();
         // return $query->row();
+
+        // $this->db->select('tbl_admin.email', 'tbl_admin.password');
+        // $this->db->from('tbl_admin');
+        // $this->db->join('patient_record', 'tbl_admin.admin_id = patient_record.patient_id');
+        // $this->db->where('tbl_admin.email', $email);
+        // $this->db->where('tbl_admin.password', $pass);
+        // $query = $this->db->get();
+        // return $query->row();
+
+
     }
 }
 
