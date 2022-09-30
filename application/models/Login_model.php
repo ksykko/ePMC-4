608d4465508660a2efd8d36a4015d0fb8d133934
ksykko <sagala.krch@gmail.com>
2022-09-30 14:34:59 +0800

    Updated sql

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -21,24 +21,6 @@ class Login_model extends CI_Model
         } else {
             return null;
         }
-        //return $query->row();
-
-        // $query = $this->db->select('ta.*', 'pr.*')
-        //     ->from('tbl_admin ta')
-        //     ->join('patient_record pr', 'ta.admin_id = pr.patient_id')
-        //     ->where('ta.email', $email)
-        //     ->where('ta.password', $pass)
-        //     ->get();
-        // return $query->row();
-
-        // $this->db->select('tbl_admin.email', 'tbl_admin.password');
-        // $this->db->from('tbl_admin');
-        // $this->db->join('patient_record', 'tbl_admin.admin_id = patient_record.patient_id');
-        // $this->db->where('tbl_admin.email', $email);
-        // $this->db->where('tbl_admin.password', $pass);
-        // $query = $this->db->get();
-        // return $query->row();
-
 
     }
 }
