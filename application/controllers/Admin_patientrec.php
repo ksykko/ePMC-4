d3be1c784e784ac6026865420fa01163461a1e77
Vengeline Angot <201910550@fit.edu.ph>
2022-09-30 13:44:10 +0800

    update on admin dashboard

--- application/controllers/Admin_patientrec.php
+++ application/controllers/Admin_patientrec.php
@@ -52,7 +52,9 @@ class Admin_patientrec extends CI_Controller
             $data['title'] = 'Admin - Patient Records | ePMC';
             $data['patient'] = $this->Admin_model->get_patient_row($id);
             $data['patients'] = $this->Admin_model->get_patient_table($page_config['per_page'], $page);
-           
+            
+
+
             $this->load->view('include-admin/dashboard-header', $data);
             $this->load->view('include-admin/dashboard-navbar', $data); // admin dashboard not yet done
             $this->load->view('admin-views/admin-patientrec-view', $data);
