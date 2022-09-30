923820f404e14d73d44af44e2b092280b282efcc
Vengeline Angot <201910550@fit.edu.ph>
2022-09-30 21:44:13 +0800

    update working add product in inventory

--- application/controllers/Admin_inventory.php
+++ application/controllers/Admin_inventory.php
@@ -11,50 +11,53 @@ class Admin_inventory extends CI_Controller {
     }
 
     public function index() {
+        if ($this->session->userdata('logged_in')) { //if logged in
+            $page_config = array(
+                'base_url' => site_url('Admin_inventory/index'),
+                'total_rows' => $this->Admin_model->get_inventory_count(), // get total number of patients 
+                'num_links' => 3,
+                'per_page' => 10,
 
-        $page_config = array(
-            'base_url' => site_url('Admin_inventory/index'),
-            'total_rows' => $this->Admin_model->get_inventory_count(), // get total number of patients 
-            'num_links' => 3,
-            'per_page' => 10,
+                'full_tag_open' => '<div class="d-flex justify-content-center"><ul class="pagination">',
+                'full_tag_close' => '</ul></div>',
 
-            'full_tag_open' => '<div class="d-flex justify-content-center"><ul class="pagination">',
-            'full_tag_close' => '</ul></div>',
+                'first_link' => FALSE,
+                'last_link' => FALSE,
 
-            'first_link' => FALSE,
-            'last_link' => FALSE,
+                'next_link' => '&raquo;',
+                'next_tag_open' => '<li class="page-item">',
+                'next_tag_close' => '</li>',
 
-            'next_link' => '&raquo;',
-            'next_tag_open' => '<li class="page-item">',
-            'next_tag_close' => '</li>',
+                'prev_link' => '&laquo;',
+                'prev_tag_open' => '<li class="page-item">',
+                'prev_tag_close' => '</li>',
 
-            'prev_link' => '&laquo;',
-            'prev_tag_open' => '<li class="page-item">',
-            'prev_tag_close' => '</li>',
+                'cur_tag_open' => '<li class="page-item active"><span class="page-link">',
+                'cur_tag_close' => '</span></li>',
 
-            'cur_tag_open' => '<li class="page-item active"><span class="page-link">',
-            'cur_tag_close' => '</span></li>',
+                'num_tag_open' => '<li class="page-item">',
+                'num_tag_close' => '</li>',
 
-            'num_tag_open' => '<li class="page-item">',
-            'num_tag_close' => '</li>',
+                'attributes' => ['class' => 'page-link']
 
-            'attributes' => ['class' => 'page-link']
+            );
 
-        );
+            $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+            $this->pagination->initialize($page_config);
 
-        $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
-        $this->pagination->initialize($page_config);
+            $id = $this->session->userdata('admin_id');
 
-        $id = $this->session->userdata('admin_id');
-
-        $data['product'] = $this->Admin_model->get_inventory_row($id);
-        $data['products'] = $this->Admin_model->get_inventory_table($page_config['per_page'], $page);
-        
-        $data['title'] = 'Inventory';
-        $this->load->view('include-admin/dashboard-header', $data);
-        $this->load->view('include-admin/dashboard-navbar', $data);
-        $this->load->view('admin-views/inventory-view', $data);
-        $this->load->view('include-website/scripts');
+            $data['product'] = $this->Admin_model->get_inventory_row($id);
+            $data['products'] = $this->Admin_model->get_inventory_table($page_config['per_page'], $page);
+            
+            $data['title'] = 'Inventory';
+            $this->load->view('include-admin/dashboard-header', $data);
+            $this->load->view('include-admin/dashboard-navbar', $data);
+            $this->load->view('admin-views/inventory-view', $data);
+            $this->load->view('include-admin/dashboard-scripts');
+        } else {
+            redirect('Login/signin');
+        }
     }
 
     public function add_product_validation()
@@ -96,7 +99,8 @@ class Admin_inventory extends CI_Controller {
                     'prod_dosage' => $this->input->post('prod_dosage'),
                     'prod_desc' => $this->input->post('prod_desc'),
                     'stock_in' => $this->input->post('stock_in'),
-                    'stock_out' => $this->input->post('stock_out')
+                    'stock_out' => $this->input->post('stock_out'),
+                    'prod_date' => date('Y-m-d H:i:s')
                 );
             }
 
@@ -120,6 +124,12 @@ class Admin_inventory extends CI_Controller {
         redirect('Admin_inventory/index');
     }
 
+    private function dd($data)
+    {
+        echo "<pre>";
+        die(var_dump($data));
+        echo "</pre>";
+    }
     
 }
 ?>
