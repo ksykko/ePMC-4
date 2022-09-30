5e300975710a269e9344d8231040cfe8b5870d6e
Vengeline Angot <201910550@fit.edu.ph>
2022-09-30 20:06:30 +0800

    updated admin navbar

--- assets/js/dashboard-header.js
+++ assets/js/dashboard-header.js
@@ -13,20 +13,20 @@ function test(){
 		"height": activeWidthNewAnimHeight + "px",
 		"width": activeWidthNewAnimWidth + "px"
 	});
-	$("#navbarSupportedContent").on("click","li",function(e){
-		$('#navbarSupportedContent ul li').removeClass("active");
-		$(this).addClass('active');
-		var activeWidthNewAnimHeight = $(this).innerHeight();
-		var activeWidthNewAnimWidth = $(this).innerWidth();
-		var itemPosNewAnimTop = $(this).position();
-		var itemPosNewAnimLeft = $(this).position();
-		$(".hori-selector").css({
-			"top":itemPosNewAnimTop.top + "px", 
-			"left":itemPosNewAnimLeft.left + "px",
-			"height": activeWidthNewAnimHeight + "px",
-			"width": activeWidthNewAnimWidth + "px"
-		});
-	});
+	// $("#navbarSupportedContent").on("click","li",function(e){
+	// 	$('#navbarSupportedContent ul li').removeClass("active");
+	// 	$(this).addClass('active');
+	// 	var activeWidthNewAnimHeight = $(this).innerHeight();
+	// 	var activeWidthNewAnimWidth = $(this).innerWidth();
+	// 	var itemPosNewAnimTop = $(this).position();
+	// 	var itemPosNewAnimLeft = $(this).position();
+	// 	$(".hori-selector").css({
+	// 		"top":itemPosNewAnimTop.top + "px", 
+	// 		"left":itemPosNewAnimLeft.left + "px",
+	// 		"height": activeWidthNewAnimHeight + "px",
+	// 		"width": activeWidthNewAnimWidth + "px"
+	// 	});
+	// });
 }
 $(document).ready(function(){
 	setTimeout(function(){ test(); });
@@ -39,16 +39,13 @@ $(".navbar-toggler").click(function(){
 	setTimeout(function(){ test(); });
 });
 
-
-
 // --------------add active class-on another-page move----------
 jQuery(document).ready(function($){
 	// Get current path and find target link
-	var path = window.location.pathname.split("/").pop();
-
+	var path = window.location.pathname;
 	// Account for home page with empty path
 	if ( path == '' ) {
-		path = 'index.html';
+		path = 'Admin/index';
 	}
 
 	var target = $('#navbarSupportedContent ul li a[href="'+path+'"]');
