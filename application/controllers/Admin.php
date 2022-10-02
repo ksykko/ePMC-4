e867050acc9636ba1acce2c7500c660763cfbe32
Vengeline Angot <201910550@fit.edu.ph>
2022-10-02 19:36:46 +0800

    updated dashboard inventory count

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -7,19 +7,23 @@ class Admin extends CI_Controller {
         $this->load->helper(['url', 'form']);
         $this->load->library(['form_validation', 'session', 'pagination']);
         $this->load->model('Admin_model');
+        $this->load->library("pagination");
     }
 
     public function index() 
-    {
+    {   
         if ($this->session->userdata('logged_in')) { //if logged in
+
             $data['title'] = 'Admin | ePMC';
-            $data['product_count'] = $this->Admin_model->get_inventory_count();
+            $id = $this->session->userdata('admin_id');
+
+            $data['product'] = $this->Admin_model->get_inventory_row($id);
+            $data['inventory_stocks'] = $this->Admin_model->get_inventory_table_contents();
             // $data['users_count'] = $this->Admin_model->get_user_count();
             // $data['patient_count'] = $this->Admin_model->get_patient_count();
             // $data['new_patient_count'] = $this->Admin_model->get_nUser_count();
 
             $this->load->view('include-admin/dashboard-header', $data);
-            //$this->load->view('admin-views/ehpaanokung');
             $this->load->view('include-admin/dashboard-navbar');
             $this->load->view('admin-views/admin-dashboard', $data);
             $this->load->view('include-admin/dashboard-scripts');
