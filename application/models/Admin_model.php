cf3667eb1215ce152bc1f0def6f9755f7ff7a948
ksykko <sagala.krch@gmail.com>
2022-10-07 21:30:58 +0800

    Deleted unused functions

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -10,46 +10,6 @@ class Admin_model extends CI_Model
         $this->load->database();
     }
 
-    var $table = "patient_record";
-    var $select_column = array("patient_id", "first_name", "middle_name", "last_name", "last_checkup");
-    var $order_column = array("patient_id", "first_name", "middle_name", "last_name", "last_checkup", null);
-    function make_query()
-    {
-        $this->db->select($this->select_column);
-        $this->db->from($this->table);
-        if (isset($_POST["search"]["value"])) {
-            $this->db->like("first_name", $_POST["search"]["value"]);
-            $this->db->or_like("last_name", $_POST["search"]["value"]);
-        }
-        if (isset($_POST["order"])) {
-            $this->db->order_by($this->order_column[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
-        } else {
-            $this->db->order_by('id', 'DESC');
-        }
-    }
-    function make_datatables()
-    {
-        $this->make_query();
-        if ($_POST["length"] != -1) {
-            $this->db->limit($_POST['length'], $_POST['start']);
-        }
-        $query = $this->db->get();
-        return $query->result();
-    }
-    function get_filtered_data()
-    {
-        $this->make_query();
-        $query = $this->db->get();
-        return $query->num_rows();
-    }
-    function get_all_data()
-    {
-        $this->db->select("*");
-        $this->db->from($this->table);
-        return $this->db->count_all_results();
-    }
-
-
     // for pagination
     public function get_patient_table()
     {
