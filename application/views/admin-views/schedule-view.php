7408dd195e46cdf4796df5f3bb0ed9fe6f394e05
Mary Angel <201910994@fit.edu.ph>
2022-10-11 15:06:20 +0800

    form validation - add sched

--- application/views/admin-views/schedule-view.php
+++ application/views/admin-views/schedule-view.php
@@ -12,26 +12,31 @@
 		<!-- POPUP FORM -->
 		<div class="popup">
 			<div class="close-btn">&times;</div>
-			<?= form_open('addSchedule'); ?>
+			<?= validation_errors(); ?>
+			<?= form_open('Admin_schedule/addSchedule'); ?>
 				<div class="form">
 					<h2>Add Schedule</h2>
 					<div class="form-element">
 						<label for="doctor">Doctor</label><br>
 						<input type="text" name="doctor_name" id="doctor_name" placeholder="Enter Doctor Name">
+						<small class="text-danger"><?= form_error('doctor_name') ?></small>
 					</div>
 					<div class="form-element">
 						<label for="specialization">Specialization</label>
 						<input type="text" name="specialization" id="specialization" placeholder="Enter Specialization">
+						<small class="text-danger"><?= form_error('specialization') ?></small>
 					</div>
 					<div class="form-element">
 						<label for="date">Set Date</label>
 						<input type="date" name="date" id="date" placeholder="Choose a Date">
+						<small class="text-danger"><?= form_error('date') ?></small>
 					</div>
 					<div class="form-element">
 						<label for="start-time">Start Time</label>
 						<label for="end-time" class="end-time">End Time</label><br>
 						<input type="time" name="start_time" id="start_time" placeholder="Start Time">
 						<input type="time" name="end_time" id="end_time" placeholder="End Time">
+						<small class="text-danger"><?= form_error('start_time') ?></small>
 					</div>
 					<!-- <div class="form-element">
 							<input type="button" value="Sun">
@@ -59,9 +64,9 @@
 				<div class="col-lg-3 list-doctors">
 					<!-- <h6>Dr. Luis Miguel Pagtakhan</h6> -->
 					<?php
-						// foreach($doctors as $doctor) {
-						// 	echo "<h6>".$doctor->doctor_name."</h6>";
-						// }
+						foreach($doctors as $doctor) {
+							echo "<h6>".$doctor->doctor_name."</h6>";
+						}
 						
 					?>
 				</div>
