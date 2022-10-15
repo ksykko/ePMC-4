29251e49f4f094ce36a602ddf07bdaf87e7602a3
Mary Angel <201910994@fit.edu.ph>
2022-10-15 23:40:35 +0800

    modal

--- application/views/admin-views/schedule-view.php
+++ application/views/admin-views/schedule-view.php
@@ -1,62 +1,95 @@
 <body>
+	
 	<div class="schedule">
+		<? foreach($get_year_db as $y) { echo $yeardb;} ?>
 		<div class="row sched-header">
 			<div class="col">
 				<h1>Schedule Overview</h1>
 			</div>
 			<div class="col addSched">
-				<button id="btn-addSched">Add New Schedule</button>
+				<button id="btn-addSched" data-bs-toggle="modal" data-bs-target="#schedule-modal">Add New Schedule</button>
 			</div>
+			
 		</div>
 
 		<!-- POPUP FORM -->
-		<div class="popup">
-			<div class="close-btn">&times;</div>
-			<?= validation_errors(); ?>
-			<?= form_open('Admin_schedule/addSchedule'); ?>
-				<div class="form">
-					<h2>Add Schedule</h2>
-					<div class="form-element">
-						<label for="doctor">Doctor</label><br>
-						<input type="text" name="doctor_name" id="doctor_name" placeholder="Enter Doctor Name">
-						<small class="text-danger"><?= form_error('doctor_name') ?></small>
+		<?= form_open('Admin_schedule/addSchedule'); ?>
+		<div id="schedule-modal" class="modal fade modal-dialog-scrollable" role="dialog" tabindex="-1">
+			<div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h4 class="modal-title ms-3 fw-bolder"> Add a Schedule</h4><button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
 					</div>
-					<div class="form-element">
-						<label for="specialization">Specialization</label>
-						<input type="text" name="specialization" id="specialization" placeholder="Enter Specialization">
-						<small class="text-danger"><?= form_error('specialization') ?></small>
-					</div>
-					<div class="form-element">
-						<label for="date">Set Date</label>
-						<input type="date" name="date" id="date" placeholder="Choose a Date">
-						<small class="text-danger"><?= form_error('date') ?></small>
-					</div>
-					<div class="form-element">
-						<label for="start-time">Start Time</label>
-						<label for="end-time" class="end-time">End Time</label><br>
-						<input type="time" name="start_time" id="start_time" placeholder="Start Time">
-						<input type="time" name="end_time" id="end_time" placeholder="End Time">
-						<small class="text-danger"><?= form_error('start_time') ?></small>
-					</div>
-					<!-- <div class="form-element">
-							<input type="button" value="Sun">
-							<input type="button" value="Mon">
-							<input type="button" value="Tue">
-							<input type="button" value="Wed">
-							<input type="button" value="Thurs">
-							<input type="button" value="Fri">
-							<input type="button" value="Sat">
-					</div> -->
-					<div class="form-element">
-						<button type="submit" name="save">Save</button>
-					</div>
-					<div class="form-element cancel">
-						<a href="#">Cancel</a>
+
+					<div class="modal-body mx-5">
+						<h5 class="heading-modal fw-semibold">Schedule Details</h5>
+						<hr size="5" />
+						<!-- DOCTOR NAME -->
+						<div class="row mt-4 mb-2">
+							<div class="col"><label class="col-form-label">Doctor Name:</label></div>
+							<div class="col">
+								<div class="input-error">
+									<div class="input-group">
+										<input type="text" class="form-control" name="doctor_name" id="doctor_name" placeholder="Enter Doctor Name">
+									</div>
+									<small class="text-danger"><?= form_error('doctor_name') ?></small>
+								</div>
+							</div>
+						</div>
+						<!-- SPECIALIZATION -->
+						<div class="row mt-4 mb-2">
+							<div class="col"><label class="col-form-label">Specialization:</label></div>
+							<div class="col">
+								<div class="input-error">
+									<div class="input-group">
+										<input type="text" class="form-control" name="specialization" id="specialization" placeholder="Enter Specialization">
+									</div>
+									<small class="text-danger"><?= form_error('specialization') ?></small>
+								</div>
+							</div>
+						</div>
+						<!-- DATE -->
+						<div class="row mt-4 mb-2">
+							<div class="col"><label class="col-form-label">Date:</label></div>
+							<div class="col">
+								<div class="input-error">
+									<div class="input-group">
+										<input type="date" class="form-control" name="date" id="date">
+									</div>
+									<small class="text-danger"><?= form_error('date') ?></small>
+								</div>
+							</div>
+						</div>
+						<!-- START TIME -->
+						<div class="row mt-4 mb-2">
+							<div class="col"><label class="col-form-label">Start Time:</label></div>
+							<div class="col">
+								<div class="input-error">
+									<div class="input-group">
+										<input type="time" class="form-control" name="start_time" id="start_time">
+									</div>
+									<small class="text-danger"><?= form_error('start_time') ?></small>
+								</div>
+							</div>
+						</div>
+						<!-- END TIME -->
+						<div class="row mt-4 mb-2">
+							<div class="col"><label class="col-form-label">End Time:</label></div>
+							<div class="col">
+								<div class="input-error">
+									<div class="input-group">
+										<input type="time" class="form-control" name="end_time" id="end_time">
+									</div>
+									<small class="text-danger"><?= form_error('end_time') ?></small>
+								</div>
+							</div>
+						</div>
+						<div class="modal-footer"><button class="btn btn-light" type="button" data-bs-dismiss="modal">Close</button><button class="btn btn-primary btn-modal" type="submit" name="Save" style="background: #3269bf;">Save</button></div>
 					</div>
 				</div>
-			<?= form_close(); ?>
-			
+			</div>
 		</div>
+		<?= form_close(); ?>
 	
 		<div class="container-fluid card shadow mb-4 sched-body">
 			<div class="row">
