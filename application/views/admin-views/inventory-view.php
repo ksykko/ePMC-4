489000c8db1e5aa91015a32bb9363b1c4de605ed
ksykko <sagala.krch@gmail.com>
2022-10-22 16:07:26 +0800

    Fixed table

--- application/views/admin-views/inventory-view.php
+++ application/views/admin-views/inventory-view.php
@@ -123,9 +123,9 @@
                                 <th class="inv-td">ID</th>
                                 <th class="inv-td">Product Name</th>
                                 <th class="inv-td">Description</th>
-                                <th class="inv-td" style='text-align: center;'>Quantity</th>
-                                <th class="inv-td" style="text-align: center" ;>Stock In</th>
-                                <th class="inv-td" style="text-align: center" ;>Stock Out</th>
+                                <th class="inv-td" style="text-align: center;">Quantity</th>
+                                <th class="inv-td" style="text-align: center;">Stock In</th>
+                                <th class="inv-td" style="text-align: center;">Stock Out</th>
                                 <th class="inv-td text-center">Action</th>
                             </tr>
                         </thead>
