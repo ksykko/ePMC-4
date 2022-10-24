22baf3a09e9ef4c9dcf389691e707fbd77af612a
Vengeline Angot <201910550@fit.edu.ph>
2022-10-25 03:35:21 +0800

    update patient dashboard with consultation history

---
+++ application/views/include-admin/patient-scripts.php
@@ -0,0 +1,39 @@
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js'></script>
+<script src="<?= base_url('/assets/js/dashboard-header.js') ?>"></script>
+<script src="<?= base_url('/assets/bootstrap/js/bootstrap.min.js') ?>"></script>
+
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#patient-diagnosis-table').DataTable({
+            "processing": true, //Feature control the processing indicator.
+            //"serverSide": true, //Feature control DataTables' server-side processing mode.
+            "order": [], //Initial no order.
+            responsive: true,
+            "ajax": {
+                url: "<?php echo site_url("Patient/datatable") ?>",
+                type: 'POST'
+            },
+
+            //Set column definition initialisation properties.
+            "columnDefs": [
+                // { "targets": [ 6 ], "orderable": false }, //set not orderable
+                { "className": "inv-td" , targets: [] },
+                { "className": "w-50", targets: [1] }
+            ]
+        });
+        
+    });
+</script>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdn.datatables.net/fixedheader/3.2.4/js/dataTables.fixedHeader.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.3.0/js/responsive.bootstrap.min.js"></script>
+
+
+
+</body>
+</html>
