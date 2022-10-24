cd6feeacfd51119c5bd7628a33c96a8ed634f0e8
Vengeline Angot <201910550@fit.edu.ph>
2022-10-25 06:05:36 +0800

    update patient record

--- application/controllers/Patient.php
+++ application/controllers/Patient.php
@@ -14,7 +14,7 @@ class Patient extends CI_Controller {
         if ($this->session->userdata('logged_in')) { //if logged in
 
             $data['title'] = 'Patient | ePMC';
-            $id = $this->session->userdata('patient_id');
+            $id = $this->session->userdata('id');
 
             $data['user_name'] = $this->session->userdata('full_name');
             $data['user_role'] = $this->session->userdata('role');
@@ -25,16 +25,10 @@ class Patient extends CI_Controller {
             $data['user_contact_no'] = $this->session->userdata('cell_no');
             $data['user_address'] = $this->session->userdata('address');
 
-            // $data['recent_diagnosis'] = $this->Patient_model->get_patient_diagnosis_row($id);
-            // $data['diagnosis'] = $this->Patient_model->get_patient_diagnosis_tbl($id);
-            // $data['product'] = $this->Admin_model->get_inventory_row($id);
-            // $data['inventory_stocks'] = $this->Admin_model->get_inventory_table_contents();
-            // $data['users_count'] = $this->Admin_model->get_useracc_count();
-            // $data['patient_count'] = $this->Admin_model->get_patient_count();
-            // $data['new_patient_count'] = $this->Admin_model->get_nUser_count();
+            $data['patient_details'] = $this->Patient_model->get_patient_details_row($id);  
 
             $this->load->view('include-admin/dashboard-header', $data);
-            $this->load->view('include-admin/dashboard-navbar');
+            $this->load->view('include-admin/dashboard-navbar', $data);
             $this->load->view('patient-views/patient-dashboard', $data);
             $this->load->view('include-admin/patient-scripts');
         }
--- application/models/Patient_model.php
+++ application/models/Patient_model.php
@@ -26,9 +26,9 @@ class Patient_model extends CI_Model
         $this->db->update('patient_record', $avatar, ['patient_id' => $id]);
     }
     
-    public function get_patient_diagnosis_row($id)
+    public function get_patient_details_row($id)
     {
-        return $this->db->get_where('patient_diagnosis', ['patient_id' => $id])->row();
+        return $this->db->get_where('patient_details', ['patient_id' => $id])->row();
     }
 
     public function get_patient_diagnosis_tbl()
