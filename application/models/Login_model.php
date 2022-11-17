dffab0e859b2cd2a32d909f1e11dc5a039603081
ksykko <sagala.krch@gmail.com>
2022-11-18 04:05:24 +0800

    Fixed Login

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -14,7 +14,7 @@ class Login_model extends CI_Model
         $query1 = $this->db->get_where('tbl_admin', ['email' => $email, 'password' => $pass]);
         $query2 = $this->db->get_where('patient_record', ['email' => $email, 'password' => $pass]);
         $query3 = $this->db->get_where('user_accounts', ['email' => $email, 'password' => $pass]);
-        $query4 = $this->db->get_where('patient_record', ['cell_no' => $email, 'password' => $pass]);
+        //$query4 = $this->db->get_where('patient_record', ['cell_no' => $email, 'password' => $pass]);
 
         if ($query1->num_rows() > 0) {
             return $query1->row();
@@ -22,8 +22,6 @@ class Login_model extends CI_Model
             return $query2->row();
         } else if ($query3->num_rows() > 0) {
             return $query3->row();
-        } else if ($query4->num_rows() > 0) {
-            return $query4->row();
         } else {
             return false;
         }
