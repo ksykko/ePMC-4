e5de710985c582b4a11bff6c34d080d459f21b73
ksykko <sagala.krch@gmail.com>
2022-11-24 05:05:00 +0800

    Added toast

--- application/views/schedule/schedule-scripts.php
+++ application/views/schedule/schedule-scripts.php
@@ -1,53 +1,48 @@
-<!-- CALENDAR JS -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<!-- Calendar -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
-<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" /> -->
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script> -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
-<!-- <script>
-	$(document).ready(function() {
-		var calendar = $('#calendar').fullCalendar({
-			editable:true,
-			header:{
-				left:'prev, today, next ',
-				center:'title',
-				right:'month, agendaWeek, agendaDay'
-			},
-            views: {
-                month: {
-                    buttonText: 'Month'
-                },
-                agendaWeek: {
-                    buttonText: 'Week'
-                },
-                agendaDay: {
-                    buttonText: 'Day'
-                }
-            },
-            selectable:true,
-            selectHelper:true
-		});
-	});
-</script> -->
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js'></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="<?= base_url('/assets/js/dashboard-header.js') ?>"></script>
+<script src="<?= base_url('/assets/bootstrap/js/bootstrap.min.js') ?>"></script>
 
-<!-- POPUP ADD SCHED -->
 <script>
-    document.getElementById("btn-addSched").addEventListener("click", function() {
-        document.querySelector(".popup").style.display = "block";
-    })
+    const toastTrigger = document.getElementById('liveToastTrigger')
+    const toastLiveExample = document.getElementById('liveToast')
 
-    document.querySelector(".close-btn").addEventListener("click", function() {
-        document.querySelector(".popup").style.display = "none";
-    })
+    var succToast = "<?= $this->session->flashdata('message') ?>";
+    var editFailed = "<?= $this->session->flashdata('edit_failed') ?>";
+    const modal_no = "user-edit-modal-" + editFailed;
+    console.log(modal_no);
 
-    document.querySelector(".cancel").addEventListener("click", function() {
-        document.querySelector(".popup").style.display = "none";
-    })
+    if (toastTrigger) {
+        if (succToast || editFailed) {
+
+
+            if (succToast == 'add_failed') {
+                $(document).ready(function() {
+                    $("#btn-add-product").modal('show');
+                });
+            }
+            if (editFailed) {
+                $(document).ready(function() {
+                    $("#" + modal_no).modal('show');
+                });
+            }
+
+            const toast = new bootstrap.Toast(toastLiveExample)
+            toast.show()
+
+        }
+    }    
 </script>
 
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdn.datatables.net/fixedheader/3.2.4/js/dataTables.fixedHeader.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.3.0/js/responsive.bootstrap.min.js"></script>
+
+
+</body>
+
+</html>
