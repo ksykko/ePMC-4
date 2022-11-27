2c7a3943f54b1df50c2dc0e8f0e74489a49f57c9
ksykko <sagala.krch@gmail.com>
2022-11-27 22:21:57 +0800

    Added footer

---
+++ application/views/include-website/footer.php
@@ -0,0 +1,7 @@
+<footer class="text-center" style="background: var(--bs-white);padding-top: 0px;padding-bottom: 0px;margin-bottom: 36px;">
+    <div class="container" style="padding-top: 0px;margin-top: 15px;">
+        <div class="row">
+            <div class="col"><small class="fw-semibold footer-text"><br />COPYRIGHT © EPMC 2022<br /></small><small class="fw-semibold footer-text">PRIVACY POLICY · TERMS OF USE<br /><br /></small></div>
+        </div>
+    </div>
+</footer>
