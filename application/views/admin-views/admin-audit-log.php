231271f7333f728357e43aa96d4fb81f96943d46
ksykko <sagala.krch@gmail.com>
2022-11-28 06:27:19 +0800

    Added Audit Log

---
+++ application/views/admin-views/admin-audit-log.php
@@ -0,0 +1,40 @@
+<div class="container-fluid patientrec-container">
+
+
+
+    <div class="d-flex mb-3">
+        <div>
+            <h1 class="patientrec-label">Audit Log</h1>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12 col-xxl-12 mb-4">
+            <div class="card shadow mb-4 p-5 pt-4 pb-5">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table id="audit_log" class="table table-hover" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th class="col-md-1">ID</th>
+                                    <!-- Include avatar -->
+                                    <th class="col-md-3 align-middle">Name</th> 
+                                    <th class="col-md-1">Role</th>
+                                    <th>Activity</th>
+                                    <th class="col-md-2">Date & Time</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+</div>
