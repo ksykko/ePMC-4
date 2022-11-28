bf76e3481211baa7572c4a26a3ea15df6c067c6b
ksykko <sagala.krch@gmail.com>
2022-11-28 22:34:27 +0800

    Fixed Audit Log

--- application/views/include-admin/audit-log-scripts.php
+++ application/views/include-admin/audit-log-scripts.php
@@ -5,10 +5,14 @@
 
 
 <script type="text/javascript">
+    var font = "font-family: 'Poppins', sans-serif;";
+
     $(document).ready(function() {
         $('#audit_log').DataTable({
             "processing": true,
-            "order": [],
+            order: [
+                [3, 'desc']
+            ],
             responsive: true,
             "ajax": {
                 url: "<?= site_url('Admin/audit_log_dt'); ?>",
@@ -20,21 +24,62 @@
                 // style the print button
                 {
                     extend: 'print',
-                    text: 'Print',
+                    text: '<i class="fas fa-print"><small style="' + font + '"> Print</small></i>',
                     className: 'btn btn-dark btn-sm',
                     exportOptions: {
-                        columns: [0, 1, 2, 3, 4]
+                        columns: [0, 1, 2, 3]
                     }
                 }
             ],
-            
+
 
             "columnDefs": [{
-                    "targets": [3],
+                    "targets": [2],
                     "orderable": false,
                 },
                 {
-                    "targets": [0, 1, 2, 3, 4],
+                    "targets": [0, 1, 2, 3],
+                    "className": "align-middle"
+                }
+
+            ],
+        });
+
+    });
+
+    $(document).ready(function() {
+        $('#patient_audit_log').DataTable({
+            "processing": true,
+            order: [
+                [3, 'desc']
+            ],
+            responsive: true,
+            "ajax": {
+                url: "<?= site_url('Admin/patient_audit_dt'); ?>",
+                type: "POST"
+            },
+            // add print button to the table
+            dom: 'Bfrtip',
+            buttons: [
+                // style the print button
+                {
+                    extend: 'print',
+                    text: '<i class="fas fa-print"><small style="' + font + '"> Print</small></i>',
+                    className: 'btn btn-dark btn-sm',
+                    exportOptions: {
+                        columns: [0, 1, 2, 3]
+                    }
+                }
+            ],
+            autoWidth: false,
+
+
+            "columnDefs": [{
+                    "targets": [2],
+                    "orderable": false,
+                },
+                {
+                    "targets": [0, 1, 2, 3],
                     "className": "align-middle"
                 }
 
